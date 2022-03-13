c8a0a1ec6f2af0ed388a6b7ac23cb04c42ec2f44
myk francis <youraveragecoder@gmail.com>
2022-03-13 20:50:24 +0300

    final commit

--- components/ChildComponent.js
+++ components/ChildComponent.js
@@ -10,7 +10,7 @@ const ChildComponent = ({ services }) => {
   function formatAMPM(date) {
     var hours = date.getHours();
     var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am';
+    var ampm = hours >= 12 ? 'Pm' : 'Am';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0'+minutes : minutes;
